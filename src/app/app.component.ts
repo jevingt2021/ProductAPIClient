6d2c2893e50529d1319b7b58b04045fa2f253dd6
jevingt2021 <jeving@nirasystems.com>
2021-04-22 17:43:12 +0530

    Section3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.post('https://localhost:44366/api/User/login', { userName: 'bob', password: 'pass' }).subscribe(response => {       console.log(response);
+  }
+    , error => {
+        console.log(error);
+      })
+  }
 }
